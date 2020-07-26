279a758710e5138b6cab83c2cd49ead193bbcc26
Kekoa Kubli <kkubli3246@gmail.com>
2020-07-25 22:15:11 -0700

    Finished Part 2

---
+++ Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1Part2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string Index()
+        {
+            return @"<b>Hello World as a string from Index!</b>";
+        }
+
+        // GET: /Test/Play
+        [HttpGet]
+        public string Play()
+        {
+            //return string as a view
+
+            return @"<b>Hello World as a string from Play!</b>";
+        }
+
+        //GET: /Test/TestView
+        [HttpGet]
+        public ActionResult TestView()
+        {
+
+            return View();
+        }
+    }
+}
